fed2fb4437d61c4cd79441b7117167578c154735
sathish s <s.sathishmechddesign@gmail.com>
2024-02-18 21:36:51 +0530

    feat: tailwind react router vite typescript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='App'>
+			<h1 className='text-2xl hover:text-sky-400 transition-colors duration-300'>Hello Vite + React!</h1>
+		</div>
+	);
+}
+
+export default App;
